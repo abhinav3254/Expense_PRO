347ce100d7cfa606376d14163d2919f2712f2867
abhinav3254 <abhinav3254@gmail.com>
2022-12-31 17:45:48 +0530

    changes

--- app/src/main/java/com/example/newapp/frames/ProfileFragment.java
+++ app/src/main/java/com/example/newapp/frames/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.newapp.frames;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,7 +9,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.newapp.ProfileActivity;
 import com.example.newapp.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.card.MaterialCardView;
 
 public class ProfileFragment extends Fragment {
 
@@ -19,6 +23,23 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_profile, container, false);
+        MaterialCardView profile_card = view.findViewById(R.id.profile_card);
+        profile_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        MaterialButton edit_profile_btn = view.findViewById(R.id.edit_profile_btn);
+        edit_profile_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
